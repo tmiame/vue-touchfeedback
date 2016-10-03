e354573f76eb6f085b92357d4e759351688fb758
tmiame <tmiame@gmail.com>
2016-10-03 16:28:00 +0900

    update gh-pages

--- dist/vue-touchfeedback.js
+++ dist/vue-touchfeedback.js
@@ -2,11 +2,6 @@
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
-/*!
- * vue-touchfeedback.js
- * (c) 2016 tmiame
- * Released under the MIT License.
- */
 ;(function () {
 
   var vueTouchFeedback = {};
